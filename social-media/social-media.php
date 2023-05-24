e87c049fe6b31d7ba7e22a545b5a6728401601b3
adriank64216 <sqat1024@gmail.com>
2023-05-24 19:30:03 +0200

    icons theme choose

--- social-media/social-media.php
+++ social-media/social-media.php
@@ -13,21 +13,40 @@ add_filter('the_content', 'social_media_icons');
 
 function social_media_icons($content){
     $html = '
-    <div>
-        <h5 style="text-align:center;">Check our social media!</h5>
-        <div style="display:flex; justify-content: space-evenly;">
-            <a href="https://'.get_option('smb_facebook', 'facebook.com').'" target="_blank"> 
-                <img width="75" height="75" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/2048px-2021_Facebook_icon.svg.png"></img>
-            </a>
-            <a href="https://'.get_option('smb_instagram', 'instagram.com').'" target="_blank"> 
-                <img width="75" height="75" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png"></img>
-            </a>
-            <a href="https://'.get_option('smb_linkedin', 'linkedin.com').'" target="_blank"> 
-                <img width="75" height="75" src="https://cdn-icons-png.flaticon.com/512/174/174857.png"></img>
-            </a>
-        </div>
-        <p style="text-align:center;">Made by social-media-buttons plugin</p>
-    </div>';
+        <div>
+            <h5 style="text-align:center;">Check our social media!</h5>
+            <div style="display:flex; justify-content: space-evenly;">
+                <a href="https://'.get_option('smb_facebook', 'facebook.com').'" target="_blank"> 
+                    <img width="75" height="75" src="'. plugin_dir_url(__FILE__) . '/dark' .'/facebook.png' .'"></img>
+                </a>
+                <a href="https://'.get_option('smb_instagram', 'instagram.com').'" target="_blank"> 
+                    <img width="75" height="75" src="'. plugin_dir_url(__FILE__) . '/dark' .'/instagram.png' .'"></img>
+                </a>
+                <a href="https://'.get_option('smb_linkedin', 'linkedin.com').'" target="_blank"> 
+                    <img width="75" height="75" src="'. plugin_dir_url(__FILE__) . '/dark' .'/linkedin.png' .'"></img>
+                </a>
+            </div>
+            <p style="text-align:center;">Made by social-media-buttons plugin</p>
+        </div>';
+    if(get_option( 'smb_icon_theme', '0') == '0'){
+        $html = '
+        <div>
+            <h5 style="text-align:center;">Check our social media!</h5>
+            <div style="display:flex; justify-content: space-evenly;">
+                <a href="https://'.get_option('smb_facebook', 'facebook.com').'" target="_blank"> 
+                    <img width="75" height="75" src="'. plugin_dir_url(__FILE__) . '/color' .'/facebook.webp' .'"></img>
+                </a>
+                <a href="https://'.get_option('smb_instagram', 'instagram.com').'" target="_blank"> 
+                    <img width="75" height="75" src="'. plugin_dir_url(__FILE__) . '/color' .'/instagram.png' .'"></img>
+                </a>
+                <a href="https://'.get_option('smb_linkedin', 'linkedin.com').'" target="_blank"> 
+                    <img width="75" height="75" src="'. plugin_dir_url(__FILE__) . '/color' .'/linkedin.png' .'"></img>
+                </a>
+            </div>
+            <p style="text-align:center;">Made by social-media-buttons plugin</p>
+        </div>';
+    }
+
     return $content.$html;
 }
 
@@ -37,6 +56,12 @@ function socialMediaAdmin() {
 
 function settings() {
     add_settings_section('smb_first_section', null, null, 'social-buttons-settings');
+
+    add_settings_field('smb_icon_theme', 'Icon theme', 'iconRadioHtml', 'social-buttons-settings', 'smb_first_section');
+    register_setting('socialmediaplugin', 'smb_icon_theme', array(
+        'sanitize_callback' => 'sanitize_text_field',
+        'default' => '0',
+    ));
 
     add_settings_field('smb_facebook', 'Facebook', 'textfieldHtml', 'social-buttons-settings', 'smb_first_section', array('mediaName' => 'facebook'));
     register_setting('socialmediaplugin', 'smb_facebook', array(
@@ -55,6 +80,15 @@ function settings() {
         'sanitize_callback' => 'sanitize_text_field',
         'default' => 'linkedin.com',
     ));
+}
+
+function iconRadioHtml() {
+    ?>
+    <label for="color">Colorful</label>
+    <input type="radio" name="smb_icon_theme" id="color" value="0" <?php echo (get_option( 'smb_icon_theme', '0') == '0') ? 'checked' : ' ' ?>>
+    <label for="dark">Dark</label>
+    <input type="radio" name="smb_icon_theme" id="dark" value="1" <?php echo (get_option( 'smb_icon_theme', '0') == '1') ? 'checked' : ' ' ?>>
+    <?php
 }
 
 function textfieldHtml($args) {
